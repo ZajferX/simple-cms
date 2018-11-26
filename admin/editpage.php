162b49aecfefb6b137730116274fedbcf59eac06
ZajferX <zajferx@gmail.com>
2018-11-26 10:00:03 +0100

    Update editpage.php

--- admin/editpage.php
+++ admin/editpage.php
@@ -29,8 +29,7 @@ if(isset($_POST['submit'])){
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo SITETITLE;?></title>
